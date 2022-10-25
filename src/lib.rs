81d69f06e4c30679f55441996c9de3376e1c0691
Ross Williams <ross@stedi.com>
2022-10-25 21:58:14 +0100

    fix: drop wee malloc to remove memory leaks
    Repeatedly calling this library causes unbounded memory growth.
    Wee-alloc has known memory leak issues.
    See https://github.com/rustwasm/wee_alloc/issues/106
    
    Rust's standard library allocator can be used instead.

--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,6 @@ use brotli;
 use wasm_bindgen::prelude::*;
 use serde::{Serialize, Deserialize};
 
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 pub fn set_panic_hook() {
     #[cfg(feature="console_error_panic_hook")]
     console_error_panic_hook::set_once();
